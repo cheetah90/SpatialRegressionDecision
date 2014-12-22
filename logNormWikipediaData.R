fadc36a2beef37660f8c3c047ffb4fe0a9be438a
Isaac J <joh12041@umn.edu>
2014-12-22 16:32:38 -0600

    Update function to merge as well. Need to still decide on how to handle zero values with logarithm

--- logNormWikipediaData.R
+++ logNormWikipediaData.R
@@ -1,12 +1,25 @@
-logNormWikipediaData <- function(dataframe, startcolnum, endcolnum, popcolnum) {
-#loops through data, log transforms per capita, sets -inf values to min
+logNormWikipediaData <- function(dfMaster, dfNewVgi, foreignKey, newVgiStartColNum, newVgiEndColNum, TotalPopColName="TotalPop", suffix="_new") {
+  #loops through data, log transforms per capita, sets -inf values to min
 
-	for(i in startcolnum:endcolnum){
-		dataframe[i] <- log10(dataframe[i]/dataframe[popcolnum])
-		realmin <- min(dataframe[dataframe[i] != -Inf, i])
-		dataframe[dataframe[i] == -Inf, i] <- realmin
-		dataframe[i] <- scale(dataframe[i])
+	#@params
+	#dfMaster: the scaled sociodemographic data frame
+	#dfMasterFK: column name within dfMaster with a suitable foreign key for joining the new data (e.g. FIPS)
+	#foreignKey: column name within both dataframes that will be used to match during the merge
+	#newVgiStartColNum: starting column number for new VGI data to be added (e.g. first column of data after FIPS)
+	#newVgiEndColNum: ending column number for new VGI data to be added
+	#TotalPopColName: dataframe column name with the population data from dfMaster
+	#suffix: suffix to append to for attributes in dfNewVgi, should be the {Language} e.g. "ZH"
+
+  dfNewVgi <- merge(dfNewVgi, dfMaster[,c[foreignKey,TotalPopColName]], by=foreignKey)
+
+	for(i in newVgiStartColNum:newVgiEndColNum){
+		dfNewVgi[i] <- log10(dfNewVgi[i]/dfNewVgi$TotalPopColName)
+		realmin <- min(dfNewVgi[dfNewVgi[i] != -Inf, i])
+		dfNewVgi[dfNewVgi[i] == -Inf, i] <- realmin
+		dfNewVgi[i] <- scale(dfNewVgi[i])
 	}
-	
-	return(dataframe)
+
+	dfMerged <- merge(dfMaster, dfNewVgi[c(1,newVgiStartColNum, newVgiEndColNum)], by=foreignKey, sort=TRUE, suffixes= c("",suffix))
+
+	return(dfMerged)
 }
