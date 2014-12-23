970be3ac7bdadbc9b96bde1489c42514c9346da2
Isaac J <joh12041@umn.edu>
2014-12-22 22:25:10 -0600

    Fix function so it works

--- logNormWikipediaData.R
+++ logNormWikipediaData.R
@@ -3,23 +3,28 @@ logNormWikipediaData <- function(dfMaster, dfNewVgi, foreignKey, newVgiStartColN
 
 	#@params
 	#dfMaster: the scaled sociodemographic data frame
-	#dfMasterFK: column name within dfMaster with a suitable foreign key for joining the new data (e.g. FIPS)
-	#foreignKey: column name within both dataframes that will be used to match during the merge
+	#dfNewVgi: data frame with new aggregated VGI data to be merged in and transformed
+	#foreignKey: column name within both dataframes that will be used to match during the merge (e.g. "FIPS")
 	#newVgiStartColNum: starting column number for new VGI data to be added (e.g. first column of data after FIPS)
 	#newVgiEndColNum: ending column number for new VGI data to be added
 	#TotalPopColName: dataframe column name with the population data from dfMaster
 	#suffix: suffix to append to for attributes in dfNewVgi, should be the {Language} e.g. "ZH"
 
-  dfNewVgi <- merge(dfNewVgi, dfMaster[,c[foreignKey,TotalPopColName]], by=foreignKey)
+  #Append total population column of each administrative area
+  dfNewVgi <- merge(dfNewVgi, dfMaster[c(foreignKey,TotalPopColName)], by=foreignKey)
 
-	for(i in newVgiStartColNum:newVgiEndColNum){
-		dfNewVgi[i] <- log10(dfNewVgi[i]/dfNewVgi$TotalPopColName)
-		realmin <- min(dfNewVgi[dfNewVgi[i] != -Inf, i])
-		dfNewVgi[dfNewVgi[i] == -Inf, i] <- realmin
-		dfNewVgi[i] <- scale(dfNewVgi[i])
+  #Takes log10 transformation, sets zero values to the minimum, and scales the resulting so the mean is set to zero and values correspond to standard deviations
+	for(i in newVgiStartColNum:newVgiEndColNum) {
+		dfNewVgi[i] <- log10(dfNewVgi[i]/dfNewVgi[,TotalPopColName])
+    if (identical(min(dfNewVgi[i]),-Inf)) {
+      realmin <- min(dfNewVgi[dfNewVgi[i] != -Inf, i])
+      dfNewVgi[dfNewVgi[i] == -Inf, i] <- realmin
+    }
+    dfNewVgi[i] <- scale(dfNewVgi[i])
 	}
 
-	dfMerged <- merge(dfMaster, dfNewVgi[c(1,newVgiStartColNum, newVgiEndColNum)], by=foreignKey, sort=TRUE, suffixes= c("",suffix))
+  #Merges newly transformed data into original dataset
+	dfMerged <- merge(dfMaster, dfNewVgi[c(newVgiStartColNum:newVgiEndColNum,match(foreignKey, names(dfNewVgi)))], by=foreignKey, suffixes= c("",suffix))
 
 	return(dfMerged)
 }
