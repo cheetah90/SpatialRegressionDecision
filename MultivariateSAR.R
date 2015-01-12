a0e8070adb87b4149077b2b891740919154a2073
Isaac J <joh12041@umn.edu>
2015-01-12 08:53:52 -0600

    Update MultivariateSAR to reflect UnivariateSAR

--- MultivariateSAR.R
+++ MultivariateSAR.R
@@ -1,16 +1,37 @@
-MultivariateSAR<-function(DVname, DataFrame, Neighbor, IVlist, Outputname){
+MultivariateSAR<-function(DataFrame, Neighbor, startDVcolnum, endDVcolnum, IVlist){
 	#Spatial Regression Decision tree on dataframe for a dependent variable and multiple independent variables at once
+		#startDVcolumn: the start column number of the Dependent Variable
+	#startDVcolumn: the ending column number of Dependent Variables
 	#DVname: Name of dependent variable column
 	#DataFrame: R data frame/table
 	#Neighbor: spatial weights matrix file from Geoda. from spdep read.gal or read.gwt2nb, left as type nb
 	#IVlist should resemble "MedAge + BachelorAb + etc." Variable length.
-	#Outputname = "file.txt"
 	
-	formulaReg<-formula(paste(DVname, "~", IVlist))
-	
-	#Run Spatial Regression Decision Function
-	
-	cat("\nRunning Spatial Regression Decision with: ", deparse(formulaReg), "\n")
-	SpatialRegressionDecision(formulaReg, DataFrame, Neighbor, Outputname)
-	
+
+	outTable <- c()
+
+	for(m in startDVcolumn:endDVcolumn)
+	{
+		DVname<-colnames(DataFrame)[m]
+		formulaReg<-formula(paste(DVname, "~", IVlist))
+
+		formulaReg
+
+		OutputName<-paste(DVname,"_x_","MultipleVariables",".txt", sep="")
+
+		#Run Spatial Regression Decision Function
+
+		cat("\nRunning Spatial Regression Decision with: ")
+		print(formulaReg)
+		cat("\n")
+
+		outTable <- SpatialRegressionDecision(formulaReg, DataFrame, Neighbor, OutputName, outTable)
+
+		cat("\n\n\n\n\n\n")
+
+	}
+
+	colnames(outTable) <- c("call", "Model_type", "Estimate of Coefficient (Pvalue)", "OLS_AdjustR^2", "SAR_PseudoR^2", "loglik/lm_loglik", "AIC/LM_AIC", "lambda_rho", "Wald_Test", "LR_Test")
+	write.csv(file=paste("MultivariateSAR.csv"),outTable)
+
 }
