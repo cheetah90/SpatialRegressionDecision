694ba8968c9e2c7b2bc8825a86051f13d1716981
Isaac J <joh12041@umn.edu>
2015-01-15 11:21:13 -0600

    Additional commands and simple log10/scale function added

--- Commands.R
+++ Commands.R
@@ -9,7 +9,32 @@ source("SpatialRegressionDecision/Prepare_MergeUGCdata.R")
 source("SpatialRegressionDecision/SpatRegrDecision_csvoutput.R")
 source("SpatialRegressionDecision/UnivariateSAR_csvoutput.R")
 
+source("AssignSignificantCode.R")
+source("DrawDistribution.R")
+source("logNormWikipediaData.R")
+source("MultivariateSAR.R")
+source("Prepare_MergeUGCdata.R")
+source("SpatRegrDecision_csvoutput.R")
+source("UnivariateSAR_csvoutput.R")
+
 usc <- read.csv("inputdata/usc_basedata.csv", header=TRUE)
 usc7nn <- read.gwt2nb("inputdata/USCounties_7nn.gwt", region.id=usc$FIPS)
 
 UnivariateSAR(DataFrame = usc, Neighbor = usc7nn, startDVcolumn = 23, endDVcolumn = 24, startIVcolnum = 7, endIVcolnum - 13)
+
+logScaleZeroesToMin <- function(dataframe, startcolnum, endcolnum) {
+  #@params
+  #dataframe: R data frame
+  #startcolnum: column index number of first column in dataframe to be transformed
+  #endcolnum: column index number of last column in dataframe to be transformed
+  
+  for(i in startcolnum:endcolnum) {
+    dataframe[i] <- log10(dataframe[i])
+    if (identical(min(dataframe[i]),-Inf)) {
+      realmin <- min(dataframe[dataframe[i] != -Inf, i])
+      dataframe[dataframe[i] == -Inf, i] <- realmin
+    }
+    dataframe[i] <- scale(dataframe[i])
+  }
+  return(dataframe)
+}
